ac9ffa6389bb7661886f2a846bb0e38a478d2cfa
IVANLEE <lee162286@gmail.com>
2026-02-11 14:25:35 +0800

    指定赛季比赛数据下载

--- update_data.py
+++ update_data.py
@@ -10,12 +10,15 @@ def main():
     print(f"当前数据库配置路径为: {loader.db_path}")
 
     # 示例 ID：勒布朗 (2544), 库里 (201939), 杜兰特 (201142)
-    test_ids = [2544, 201939, 201142]
+    # test_ids = [2544, 201939, 201142]
 
-    loader.fetch_player_career(player_ids=test_ids)
+    # loader.fetch_player_career(player_ids=test_ids)
 
     # 获取所有球员信息
     # loader.fetch_all_players()
+
+    # 获取某一年的比赛数据
+    loader.fetch_games(season="1990-91")
 
 
 if __name__ == "__main__":
